fdd8f32c42187df00a4f49eeea49e94547b54cc2
Don Kirkby <donkirkby@gmail.com>
2020-08-19 21:30:32 -0700

    Close #1 by adding GUI for Spline.

--- shibumi/spline/display.py
+++ shibumi/spline/display.py
@@ -3,11 +3,37 @@ from pathlib import Path
 import numpy as np
 from PySide2.QtCore import QSize
 from PySide2.QtCore import Qt
-from PySide2.QtGui import QImage, QPixmap
-from PySide2.QtWidgets import QGraphicsPixmapItem
-from zero_play.game_display import GameDisplay
+from PySide2.QtGui import QImage, QPixmap, QFont
+from PySide2.QtWidgets import QGraphicsPixmapItem, QGraphicsSceneHoverEvent, QGraphicsSceneMouseEvent
+from zero_play.game_display import GameDisplay, center_text_item
 
 from shibumi.spline.game import SplineGame
+
+
+class GraphicsShibumiPieceItem(QGraphicsPixmapItem):
+    def __init__(self, height: int, row: int, column: int, hover_listener=None):
+        super().__init__()
+        self.height = height
+        self.row = row
+        self.column = column
+        self.setAcceptHoverEvents(True)
+        self.hover_listener = hover_listener
+
+    def hoverEnterEvent(self, event: QGraphicsSceneHoverEvent):
+        super().hoverEnterEvent(event)
+        self.hover_listener.on_hover_enter(self)
+
+    def hoverLeaveEvent(self, event: QGraphicsSceneHoverEvent):
+        super().hoverLeaveEvent(event)
+        self.hover_listener.on_hover_leave(self)
+
+    def mousePressEvent(self, event: QGraphicsSceneMouseEvent):
+        super().mousePressEvent(event)
+        self.hover_listener.on_click(self)
+
+    def __repr__(self):
+        return (f'GraphicsShibumiPieceItem({self.height}, {self.row}, '
+                f'{self.column})')
 
 
 class SplineDisplay(GameDisplay):
@@ -27,53 +53,125 @@ class SplineDisplay(GameDisplay):
             for row in range(self.game.SIZE - height):
                 item_row = []
                 for column in range(self.game.SIZE - height):
-                    item = self.scene.addPixmap(self.white_scaled)
-                    # item.setVisible(False)
+                    item = GraphicsShibumiPieceItem(height, row, column, self)
+                    self.scene.addItem(item)
                     item_row.append(item)
                 item_level.append(item_row)
             self.item_levels.append(item_level)
+        self.player_item = self.scene.addPixmap(QPixmap())
+        self.move_text = self.scene.addSimpleText('')
+        self.text_x = self.text_y = 0
 
     def update(self, board: np.ndarray):
         self.current_board = board
+        valid_moves = self.game.get_valid_moves(board)
+        is_ended = self.game.is_ended(board)
+        for level, item_level in zip(self.game.get_levels(self.current_board),
+                                     self.item_levels):
+            for row_pieces, row_items in zip(level, item_level):
+                piece_item: QGraphicsPixmapItem
+                for piece, piece_item in zip(row_pieces, row_items):
+                    move_index = self.game.get_index(piece_item.height,
+                                                     piece_item.row,
+                                                     piece_item.column)
+                    is_valid = valid_moves[move_index] and not is_ended
+                    piece_item.setVisible(piece != self.game.NO_PLAYER or
+                                          is_valid)
+                    if piece == self.game.WHITE:
+                        piece_item.setPixmap(self.white_scaled)
+                    else:
+                        piece_item.setPixmap(self.black_scaled)
+                    piece_item.setOpacity(0.001
+                                          if piece == self.game.NO_PLAYER
+                                          else 1)
+        displayed_player = None
+        if is_ended:
+            if self.game.is_win(board, self.game.WHITE):
+                self.update_move_text('wins')
+                displayed_player = self.game.WHITE
+            elif self.game.is_win(board, self.game.BLACK):
+                self.update_move_text('wins')
+                displayed_player = self.game.BLACK
+            else:
+                self.update_move_text('draw')
+        else:
+            displayed_player = self.game.get_active_player(board)
+            self.update_move_text(self.choose_active_text())
+        if displayed_player == self.game.WHITE:
+            self.player_item.setPixmap(self.white_scaled)
+        elif displayed_player == self.game.BLACK:
+            self.player_item.setPixmap(self.black_scaled)
+        self.player_item.setVisible(displayed_player is not None)
+
+    def update_move_text(self, text: str = None):
+        if text is not None:
+            self.move_text.setText(text)
+        center_text_item(self.move_text, self.text_x, self.text_y)
 
     def resize(self, view_size: QSize):
         super().resize(view_size)
         self.game: SplineGame
-        scaled_pixmap = self.background_pixmap.scaled(self.scene.width(),
+        scaled_pixmap = self.background_pixmap.scaled(self.scene.width() // 1.25,
                                                       self.scene.height(),
                                                       Qt.KeepAspectRatio,
                                                       Qt.SmoothTransformation)
         self.background_item.setPixmap(scaled_pixmap)
-        self.background_item.setPos(
-            (self.scene.width() - scaled_pixmap.width()) // 2,
-            (self.scene.height() - scaled_pixmap.height()) // 2)
-        self.black_scaled = self.black_pixmap.scaled(self.scene.width() // 4,
-                                                     self.scene.height() // 4,
+        display_width = scaled_pixmap.width() // .795
+        x0 = (self.scene.width() - display_width) // 2
+        y0 = (self.scene.height() - scaled_pixmap.height()) // 2
+        self.background_item.setPos(x0, y0)
+        self.black_scaled = self.black_pixmap.scaled(display_width // 4.9,
+                                                     display_width // 4.9,
                                                      Qt.KeepAspectRatio,
                                                      Qt.SmoothTransformation)
-        self.white_scaled = self.white_pixmap.scaled(self.scene.width() // 4,
-                                                     self.scene.height() // 4,
+        self.white_scaled = self.white_pixmap.scaled(display_width // 4.9,
+                                                     display_width // 4.9,
                                                      Qt.KeepAspectRatio,
                                                      Qt.SmoothTransformation)
         cell_size = scaled_pixmap.width() // 4.5
-        x0 = scaled_pixmap.width() // 21
-        y0 = scaled_pixmap.height() // 22
-        for height, (level, item_level) in enumerate(zip(
-                self.game.get_levels(self.current_board),
-                self.item_levels)):
-            for row, (row_pieces, row_items) in enumerate(zip(level,
-                                                              item_level)):
+        x0 += scaled_pixmap.width() // 22
+        y0 += scaled_pixmap.height() // 22
+        for height, level in enumerate(self.item_levels):
+            for row, row_items in enumerate(level):
                 row = self.game.SIZE - row - height - 1
                 piece_item: QGraphicsPixmapItem
-                for column, (piece, piece_item) in enumerate(zip(row_pieces,
-                                                                 row_items)):
+                for column, piece_item in enumerate(row_items):
                     piece_item.setPos(x0+(height + 2*column)*cell_size // 2,
                                       y0+(height + 2*row)*cell_size // 2)
-                    piece_item.setVisible(piece != self.game.NO_PLAYER)
-                    if piece == self.game.WHITE:
-                        piece_item.setPixmap(self.white_scaled)
-                    elif piece == self.game.BLACK:
-                        piece_item.setPixmap(self.black_scaled)
+        self.player_item.setPos(x0 + scaled_pixmap.width() // 1.032,
+                                y0 + scaled_pixmap.height() // 3.05)
+        font = QFont(self.default_font)
+        font.setPointSize(cell_size * 2 // 9)
+        self.move_text.setFont(font)
+        self.text_x = x0 + scaled_pixmap.width() + cell_size // 2.3
+        self.text_y = y0 + scaled_pixmap.height() // 1.65
+        self.update(self.current_board)
+
+    def on_hover_enter(self, piece_item: GraphicsShibumiPieceItem):
+        levels = self.game.get_levels(self.current_board)
+        piece = levels[piece_item.height][piece_item.row][piece_item.column]
+        if piece != self.game.NO_PLAYER:
+            return
+        player = self.game.get_active_player(self.current_board)
+        if player == self.game.BLACK:
+            piece_item.setPixmap(self.black_scaled)
+        else:
+            piece_item.setPixmap(self.white_scaled)
+        piece_item.setOpacity(0.5)
+
+    def on_hover_leave(self, piece_item: GraphicsShibumiPieceItem):
+        levels = self.game.get_levels(self.current_board)
+        piece = levels[piece_item.height][piece_item.row][piece_item.column]
+        if piece == self.game.NO_PLAYER:
+            piece_item.setOpacity(0.001)
+
+    def on_click(self, piece_item: GraphicsShibumiPieceItem):
+        move_index = self.game.get_index(piece_item.height,
+                                         piece_item.row,
+                                         piece_item.column)
+        valid_moves = self.game.get_valid_moves(self.current_board)
+        if valid_moves[move_index]:
+            self.update(self.game.make_move(self.current_board, move_index))
 
     @staticmethod
     def load_pixmap(name: str, size: QSize = None) -> QPixmap:
--- tests/spline/test_spline_display.py
+++ tests/spline/test_spline_display.py
@@ -1,20 +1,40 @@
 from PySide2.QtCore import QSize
-from PySide2.QtGui import QPainter
+from PySide2.QtGui import QPainter, QFont
+from PySide2.QtWidgets import QGraphicsScene
+from zero_play.game_display import center_text_item
 from zero_play.pixmap_differ import PixmapDiffer
 
 from shibumi.spline.display import SplineDisplay
 
 
+def add_text(scene: QGraphicsScene, text: str, x: int, y: int, font_size: int):
+    font = QFont(SplineDisplay.default_font)
+    font.setPointSize(font_size)
+    text_item = scene.addSimpleText(text, font)
+    center_text_item(text_item, x, y)
+
+
+# noinspection DuplicatedCode
 def test_empty(pixmap_differ: PixmapDiffer):
     actual: QPainter
     expected: QPainter
-    with pixmap_differ.create_painters(240, 240, 'display_splinex') as (actual, expected):
-        expected.drawPixmap(1, 0, SplineDisplay.load_pixmap('board-1.png',
-                                                            QSize(240, 240)))
+    with pixmap_differ.create_painters(300, 240, 'spline_empty') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(black_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
 
         display = SplineDisplay()
 
-        display.resize(QSize(240, 240))
+        display.resize(QSize(300, 240))
 
         display.scene.render(actual)
 
@@ -23,26 +43,31 @@ def test_empty(pixmap_differ: PixmapDiffer):
 def test_first_level(pixmap_differ: PixmapDiffer):
     actual: QPainter
     expected: QPainter
-    with pixmap_differ.create_painters(240, 240, 'spline_first_level') as (
+    with pixmap_differ.create_painters(300, 240, 'spline_first_level') as (
             actual,
             expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
         white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
                                                QSize(60, 60))
         black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
                                                QSize(60, 60))
 
-        expected.drawPixmap(1, 0, SplineDisplay.load_pixmap('board-1.png',
-                                                            QSize(240, 240)))
-        expected.drawPixmap(11, 10, white_ball)
-        expected.drawPixmap(63, 10, black_ball)
-        expected.drawPixmap(167, 62, white_ball)
+        expected_scene.addPixmap(white_ball).setPos(11, 10)
+        expected_scene.addPixmap(black_ball).setPos(63, 10)
+        expected_scene.addPixmap(black_ball).setPos(167, 62)
+        expected_scene.addPixmap(white_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
 
         display = SplineDisplay()
         display.update(display.game.create_board("""\
   A C E G
 7 W B . . 7
 
-5 . . . W 5
+5 . . . B 5
 
 3 . . . . 3
 
@@ -50,7 +75,7 @@ def test_first_level(pixmap_differ: PixmapDiffer):
   A C E G
 """))
 
-        display.resize(QSize(240, 240))
+        display.resize(QSize(300, 240))
 
         display.scene.render(actual)
 
@@ -59,21 +84,26 @@ def test_first_level(pixmap_differ: PixmapDiffer):
 def test_second_level(pixmap_differ: PixmapDiffer):
     actual: QPainter
     expected: QPainter
-    with pixmap_differ.create_painters(240, 240,
-                                       'spline_second_level',
-                                       max_diff=60) as (actual, expected):
+    with pixmap_differ.create_painters(300, 240, 'spline_second_level') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
         white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
                                                QSize(60, 60))
         black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
                                                QSize(60, 60))
 
-        expected.drawPixmap(1, 0, SplineDisplay.load_pixmap('board-1.png',
-                                                            QSize(240, 240)))
-        expected.drawPixmap(11, 10, white_ball)
-        expected.drawPixmap(63, 10, black_ball)
-        expected.drawPixmap(11, 62, black_ball)
-        expected.drawPixmap(63, 62, white_ball)
-        expected.drawPixmap(37, 36, white_ball)
+        expected_scene.addPixmap(black_ball).setPos(11, 62)
+        expected_scene.addPixmap(white_ball).setPos(63, 62)
+        expected_scene.addPixmap(white_ball).setPos(11, 10)
+        expected_scene.addPixmap(black_ball).setPos(63, 10)
+        expected_scene.addPixmap(black_ball).setPos(37, 36)
+        expected_scene.addPixmap(white_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
 
         display = SplineDisplay()
         display.update(display.game.create_board("""\
@@ -87,7 +117,7 @@ def test_second_level(pixmap_differ: PixmapDiffer):
 1 . . . . 1
   A C E G
    B D F
- 6 W . . 6
+ 6 B . . 6
 
  4 . . . 4
 
@@ -95,6 +125,307 @@ def test_second_level(pixmap_differ: PixmapDiffer):
    B D F
 """))
 
-        display.resize(QSize(240, 240))
+        display.resize(QSize(300, 240))
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_resize_wide(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(400, 240, 'spline_resize_wide') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(51, 0)
+        white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
+                                               QSize(60, 60))
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(white_ball).setPos(61, 10)
+        expected_scene.addPixmap(black_ball).setPos(113, 10)
+        expected_scene.addPixmap(black_ball).setPos(217, 62)
+        expected_scene.addPixmap(white_ball).setPos(290, 88)
+        add_text(expected_scene, 'to move', 320, 155, 11)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        display.update(display.game.create_board("""\
+  A C E G
+7 W B . . 7
+
+5 . . . B 5
+
+3 . . . . 3
+
+1 . . . . 1
+  A C E G
+"""))
+
+        display.resize(QSize(400, 240))
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_resize_narrow(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(150, 240, 'spline_resize_narrow') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 150, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(120, 123))).setPos(0, 59)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(30, 30))
+
+        expected_scene.addPixmap(black_ball).setPos(121, 103)
+        add_text(expected_scene, f'to move', 136, 137, 5)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+
+        display.resize(QSize(150, 240))
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_hover_enter(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(300, 240, 'spline_hover_enter') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(black_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        new_piece = expected_scene.addPixmap(black_ball)
+        new_piece.setPos(115, 114)
+        new_piece.setOpacity(0.5)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        height = 0
+        row = 1
+        column = 2
+        piece_item = display.item_levels[height][row][column]
+
+        display.resize(QSize(300, 240))
+        display.on_hover_enter(piece_item)
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_click(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(300, 240, 'spline_click') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+        white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(black_ball).setPos(115, 114)
+        expected_scene.addPixmap(white_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        height = 0
+        row = 1
+        column = 2
+        piece_item = display.item_levels[height][row][column]
+
+        display.resize(QSize(300, 240))
+        display.on_click(piece_item)
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_hover_leave(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(300, 240, 'spline_hover_leave') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+        expected_scene.addPixmap(black_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        height = 0
+        row = 1
+        column = 2
+        piece_item = display.item_levels[height][row][column]
+
+        display.resize(QSize(300, 240))
+        display.on_hover_enter(piece_item)
+        display.on_hover_leave(piece_item)
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_hover_enter_existing(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(300, 240, 'spline_hover_enter_existing') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+        white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(black_ball).setPos(115, 114)
+        expected_scene.addPixmap(white_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        display.update(display.game.create_board("""\
+  A C E G
+7 . . . . 7
+
+5 . . . . 5
+
+3 . . B . 3
+
+1 . . . . 1
+  A C E G
+"""))
+        height = 0
+        row = 1
+        column = 2
+        piece_item = display.item_levels[height][row][column]
+
+        display.resize(QSize(300, 240))
+        display.on_hover_enter(piece_item)
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_hover_leave_existing(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(300, 240, 'spline_hover_leave_existing') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+        white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(black_ball).setPos(115, 114)
+        expected_scene.addPixmap(white_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        display.update(display.game.create_board("""\
+  A C E G
+7 . . . . 7
+
+5 . . . . 5
+
+3 . . B . 3
+
+1 . . . . 1
+  A C E G
+"""))
+        height = 0
+        row = 1
+        column = 2
+        piece_item = display.item_levels[height][row][column]
+
+        display.resize(QSize(300, 240))
+        display.on_hover_leave(piece_item)
+
+        display.scene.render(actual)
+
+
+# noinspection DuplicatedCode
+def test_double_update(pixmap_differ: PixmapDiffer):
+    actual: QPainter
+    expected: QPainter
+    with pixmap_differ.create_painters(300, 240, 'spline_double_update') as (
+            actual,
+            expected):
+        expected_scene = QGraphicsScene(0, 0, 300, 240)
+        expected_scene.addPixmap(
+            SplineDisplay.load_pixmap('board-1.png',
+                                      QSize(240, 240))).setPos(1, 0)
+        white_ball = SplineDisplay.load_pixmap('ball-w-shadow-1.png',
+                                               QSize(60, 60))
+        black_ball = SplineDisplay.load_pixmap('ball-b-shadow-1.png',
+                                               QSize(60, 60))
+
+        expected_scene.addPixmap(black_ball).setPos(11, 10)
+        expected_scene.addPixmap(white_ball).setPos(63, 10)
+        expected_scene.addPixmap(black_ball).setPos(115, 62)
+        expected_scene.addPixmap(white_ball).setPos(240, 88)
+        add_text(expected_scene, 'to move', 270, 155, 11)
+        expected_scene.render(expected)
+
+        display = SplineDisplay()
+        display.resize(QSize(300, 240))
+        display.update(display.game.create_board("""\
+  A C E G
+7 W B . . 7
+
+5 . . . W 5
+
+3 . . . . 3
+
+1 . . . . 1
+  A C E G
+"""))
+        display.update(display.game.create_board("""\
+  A C E G
+7 B W . . 7
+
+5 . . B . 5
+
+3 . . . . 3
+
+1 . . . . 1
+  A C E G
+"""))
 
         display.scene.render(actual)
