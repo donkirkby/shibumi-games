869f0b357ec6bbef7a8215cc7943e549ad0424e6
Don Kirkby <donkirkby@gmail.com>
2020-10-23 13:52:59 -0700

    Add Shibumi version to about box.

--- shibumi/spline/display.py
+++ shibumi/spline/display.py
@@ -1,5 +1,6 @@
 import typing
 
+import shibumi
 from shibumi.shibumi_display import ShibumiDisplay
 from shibumi.spline.game import SplineState
 
@@ -13,4 +14,5 @@ class SplineDisplay(ShibumiDisplay):
     @property
     def credit_pairs(self) -> typing.Iterable[typing.Tuple[str, str]]:
         return [('Spline Game:', 'Néstor Romeral Andrés'),
-                ('Spline Implementation:', 'Don Kirkby')]
+                ('Spline Implementation:', 'Don Kirkby'),
+                ('Shibumi Version', shibumi.__version__)]
